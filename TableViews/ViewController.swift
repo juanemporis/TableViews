2c11794199cd25a1e79328a7564ab0a90ec2d150
juan castañeda <juanemporis123>
2022-04-28 17:42:10 -0500

    Initial Commit

---
+++ TableViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViews
+//
+//  Created by wendy manrique on 28/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
