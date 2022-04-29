a1d1583eae59ad5b06956a6a3d0915e441722dd4
juan castañeda <juanemporis123>
2022-04-28 19:32:05 -0500

    Se añadieron nombres de países a las tablas usando la función TABLEVIEW

--- TableViews/ViewController.swift
+++ TableViews/ViewController.swift
@@ -11,10 +11,29 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    private let  myCountries = ["España","Mexico","Peru","Colombia","Argentina","Venezuela","EE.UU"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
     }
 
+ 
+}
 
+extension ViewController : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myCountries.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "mycell")
+        if cell == nil{
+            cell = UITableViewCell(style: .default, reuseIdentifier: "mycell")
+        }
+        
+        cell!.textLabel?.text = myCountries[indexPath.row]
+        return cell!
+    }
 }
 
